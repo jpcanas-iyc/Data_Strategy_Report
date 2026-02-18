eb1d284d9d5c985918430e0b8f11c7194b3c2132
jpcanas-iyc <auxiliardatos@incolmotos-yamaha.com.co>
2026-02-18 11:38:34 -0500

    Cambios img

--- enhanced_flipbook_generator copy 4.py
+++ enhanced_flipbook_generator copy 4.py
@@ -461,8 +461,8 @@ def generar_flipbook(df, output="output/flipbook.html"):
 
 
     pagina_imagen = f"""
-    <div class="double full-image-page"
-        style="background-image: url('../img/xsr900blancorojo.jpg');">
+    <div class="double full-image-page">
+        <img src="img/IMG_0283.jpeg" alt="img_0283">
     </div>
     """
 
